d5d090e46478bd379d2522c64d4bd03ee5bd6e1d
John Houston <john@biocogniv.com>
2021-07-16 11:47:53 -0600

    Add Micronaut; Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,90 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
+import com.google.protobuf.gradle.*
 plugins {
-    kotlin("jvm") version "1.5.10"
-    application
+    id("org.jetbrains.kotlin.jvm") version "1.4.32"
+    id("org.jetbrains.kotlin.kapt") version "1.4.32"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("io.micronaut.application") version "1.5.3"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.32"
+    id("com.google.protobuf") version "0.8.15"
 }
 
-group = "me.john"
-version = "1.0-SNAPSHOT"
+version = "0.1"
+group = "com.bpsw.cabs"
 
+val kotlinVersion=project.properties.get("kotlinVersion")
 repositories {
-    jcenter()
     mavenCentral()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
+}
+
+micronaut {
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("com.bpsw.cabs.*")
+    }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("io.ktor:ktor-server-netty:1.5.2")
-    implementation("io.ktor:ktor-html-builder:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.grpc:micronaut-grpc-runtime")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    implementation("io.micronaut:micronaut-validation")
 
-    implementation("com.h2database:h2:1.4.200")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    testImplementation("io.micronaut:micronaut-http-client")
+
 }
 
-tasks.test {
-    useJUnit()
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
-}
 
 application {
-    mainClassName = "ServerKt"
+    mainClass.set("com.bpsw.cabs.ApplicationKt")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("1.8")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+
+}
+sourceSets {
+    main {
+        java {
+            srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/java")
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.17.2"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.38.0"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                // Apply the "grpc" plugin whose spec is defined above, without options.
+                id("grpc")
+            }
+        }
+    }
 }
--- src/main/kotlin/com/bpsw/cabs/view/CabsPageRep.kt
+++ src/main/kotlin/com/bpsw/cabs/view/CabsPageRep.kt
@@ -1,4 +1,4 @@
 package com.bpsw.cabs.view
 
-class CabsPageRep() : AbstractRep {
+class CabsPageRep() : AbstractRep() {
 }
