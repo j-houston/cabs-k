ac1c9a17c892a3baeaa36d77dae4ee0be9a8a30e
John Houston <j-houston@users.noreply.github.com>
2021-07-16 10:52:44 -0600

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "me.john"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-netty:1.5.2")
+    implementation("io.ktor:ktor-html-builder:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+
+    implementation("com.h2database:h2:1.4.200")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClassName = "ServerKt"
+}
